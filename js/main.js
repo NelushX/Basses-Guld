d244f3fa3d893cb4551e6e5665f04365bd164970
Stefan Hallberg <stefanhalllberg@live.se>
2019-12-05 10:44:00 +0100

    flytt

--- js/main.js
+++ js/main.js
@@ -33,18 +33,168 @@ $(document).ready(function() {
         this.image3 = image3;
         this.image4 = image4;
     }
-    // sökfunktion
-    $("#button").on("click", function() {
-        let input = $("#search").val();
-        $("//divarna").each( function() {
-            let text = $(this).text();
-            if (text.indexOf(input)!=-1) {
-                $(this).parent().parent().show();
-            }
-            else {
-                $(this).parent().parent().hide();
-            }
-        });
-    });​
-    //
+    
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// sökfunktion
+$("#button").on("click", function() {
+    let input = $("#search").val();
+    $("//divarna").each( function() {
+        let text = $(this).text();
+        if (text.indexOf(input)!=-1) {
+            $(this).parent().parent().show();
+        }
+        else {
+            $(this).parent().parent().hide();
+        }
+    });
+});​ 
+//
