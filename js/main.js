538d87001a66f504a06ec33ee530cdd986f992b3
NelushX <hjalle1990@hotmail.com>
2019-12-13 15:54:59 +0100

    magnus

--- js/main.js
+++ js/main.js
@@ -124,44 +124,45 @@ $(document).ready(function() {
     //     }
     // });
     
-    let searchResult = [];
+
+    // let searchResult = [];    // Vad gör den här?
 
     $("#input").on("keypress", function() {
         $("#productlist").empty();
-        //sökfunktion
+
         let search = $("#input").val();
-        
-        productlist.forEach(function(value) {
+        $("#searchstring").html(search);
+
+        productPage.forEach(function(value) {
             if(value.brand.indexOf(search) >= 0 || value.price.toString().indexOf(search) >= 0) {
-                searchResult.push(value);
-                // adderar bilder på de sökta objekten
+                // searchResult.push(value);      // Vad gör den här?
+
                 let productContainer = $("<div>").addClass("productContainer prodlink col-6 col-lg-3").attr("id", value.artnr).appendTo($("#productlist"));
                 let imageContainer = $("<div>").addClass("imageContainer").appendTo(productContainer);
-                let image = $("<img>").attr("src", value.image1).attr("alt", value.name)
-                //hover bilder   
+                let image = $("<img>").attr("src", "../" + value.image1).attr("alt", value.name)
                     .mouseover(function() {
-                        image.attr("src", value.image2);
+                        image.attr("src", "../" + value.image2);
                     })
                     .mouseout(function() {
-                        image.attr("src", value.image1);
+                        image.attr("src", "../" + value.image1);
                     })
                     .appendTo(imageContainer);
 
-                // skapar upp information
                 let infoContainer = $("<div>").addClass("infoContainer mt-3").appendTo(productContainer);
                 let brand = $("<p>").html("<b>" + value.brand + "</b>").addClass("pBrand").appendTo(infoContainer);
                 let name = $("<p>").html(value.name).addClass("pName").appendTo(infoContainer);
                 let price = $("<p>").html("<b>" + value.price + " kr" + "</b>").addClass("pPrice").appendTo(infoContainer);
 
                 localStorage.setItem("search", JSON.stringify(search));
-               
             }
         });
             //window.open("html/search.html","_searchpage");
+
+        $(".prodlink").on("click", function() {
+            window.open("product.html?id=" + $(this).attr("id"), "_self");
+        });
     });
     
-
-
 
     // Shoppingcart
     let basketlist = JSON.parse(localStorage.getItem("prodList"));
