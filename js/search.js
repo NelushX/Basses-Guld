dbfa30ada24dd90df4cdaded508e2ad105ab274b
NelushX <hjalle1990@hotmail.com>
2019-12-19 10:02:53 +0100

    magnus

--- js/search.js
+++ js/search.js
@@ -1,5 +1,47 @@
 $(document).ready(function() {
 
+    if (localStorage.getItem("currentBasket")) {
+    
+        let productInfo = JSON.parse(localStorage.getItem("currentBasket"));
+
+        for (let i = 0; i < productInfo.length; i++) {
+            
+            //Setting modal-body div attributes
+            let modalBody = $(".modal-body").addClass("container");
+            let modalRow = $("<div>").attr("id", "basketObject").addClass("row align-items-center");
+            let modalCol1 = $("<div>").addClass("col-5").attr("id", "imageCol");
+            let modalCol2 = $("<div>").addClass("col-4").attr("id", "infoCol");
+            let modalCol3 = $("<div>").addClass("col-3").attr("id", "qtyCol");
+            modalRow.appendTo(modalBody);
+            modalCol1.appendTo(modalRow);
+            modalCol2.appendTo(modalRow);
+            modalCol3.appendTo(modalRow);
+
+            // Image
+            let modalImg = $("<img>").attr("src", productInfo[i].image1).attr("class", "img-fluid");
+            modalImg.appendTo(modalCol1);
+
+            //productInfo
+            $("<p>").html(productInfo[i].name).attr("id", "basketName").appendTo(modalCol2);
+
+            $("<p>").html(productInfo[i].brand).attr("id", "basketBrand").appendTo(modalCol2);
+
+            $("<p>").html("<b>" + productInfo[i].price + "kr" + "</b>").attr("id", "basketPrice").appendTo(modalCol2);
+
+            $("<p>").html("&times;").attr("id", "basketRemove").appendTo(modalCol3);
+
+            $("<p>").html("Antal: " + productInfo[i].quantity).attr("id", "basketQuantity").appendTo(modalCol3);
+
+            $("<button>").html("-").addClass("btn btn-dark").attr("id", "basketDecrease").appendTo(modalCol3);
+
+            $("<button>").html("+").addClass("btn btn-dark").attr("id", "basketIncrease").appendTo(modalCol3);
+
+            let findTotalPrice = $("#modalTotalPrice");
+            findTotalPrice.html("Totalbelopp: " + productInfo[i].price + "kr").attr("id", "modalTotalPriceH5").appendTo(findTotalPrice);
+        }
+    }
+
+    
     let productPage = JSON.parse(localStorage.getItem("prodList"));
     let searchitem = JSON.parse(localStorage.getItem("search"));
 
